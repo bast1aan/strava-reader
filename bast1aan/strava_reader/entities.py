06958e08f31870d06f92843d155fdfa5504aa2ae
Bastiaan Welmers <bastiaan@welmers.net>
2026-08-09 20:06:14 +0200

    Add internal type ApiActivity
    flattened out version of the strava_model DetailedActivity

--- bast1aan/strava_reader/entities.py
+++ bast1aan/strava_reader/entities.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 from contextlib import AbstractContextManager
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import Sequence
+from typing import Sequence, Literal
 
 
 class Store(AbstractContextManager):
@@ -108,3 +108,305 @@ class Activity:
 	time_from_timer: timedelta # 'Tijd op de timer',
 	cyclus_count: int #'Totaalaantal cycli',
 	media: str # 'Media',
+
+
+type SportType = Literal[
+	"AlpineSki",
+	"BackcountrySki",
+	"Badminton",
+	"Basketball",
+	"Canoeing",
+	"Cricket",
+	"Crossfit",
+	"Dance",
+	"EBikeRide",
+	"Elliptical",
+	"EMountainBikeRide",
+	"Golf",
+	"GravelRide",
+	"Handcycle",
+	"HighIntensityIntervalTraining",
+	"Hike",
+	"IceSkate",
+	"InlineSkate",
+	"Kayaking",
+	"Kitesurf",
+	"MountainBikeRide",
+	"NordicSki",
+	"Padel",
+	"PhysicalTherapy",
+	"Pickleball",
+	"Pilates",
+	"Racquetball",
+	"Ride",
+	"RockClimbing",
+	"RollerSki",
+	"Rowing",
+	"Run",
+	"Sail",
+	"Skateboard",
+	"Snowboard",
+	"Snowshoe",
+	"Soccer",
+	"Squash",
+	"StairStepper",
+	"StandUpPaddling",
+	"Surfing",
+	"Swim",
+	"TableTennis",
+	"Tennis",
+	"TrailRun",
+	"Velomobile",
+	"VirtualRide",
+	"VirtualRow",
+	"VirtualRun",
+	"Volleyball",
+	"Walk",
+	"WeightTraining",
+	"Wheelchair",
+	"Windsurf",
+	"Workout",
+	"Yoga",
+]
+
+
+type ActivityType = Literal[
+	"AlpineSki",
+	"BackcountrySki",
+	"Canoeing",
+	"Crossfit",
+	"EBikeRide",
+	"Elliptical",
+	"Golf",
+	"Handcycle",
+	"Hike",
+	"IceSkate",
+	"InlineSkate",
+	"Kayaking",
+	"Kitesurf",
+	"NordicSki",
+	"Ride",
+	"RockClimbing",
+	"RollerSki",
+	"Rowing",
+	"Run",
+	"Sail",
+	"Skateboard",
+	"Snowboard",
+	"Snowshoe",
+	"Soccer",
+	"StairStepper",
+	"StandUpPaddling",
+	"Surfing",
+	"Swim",
+	"Velomobile",
+	"VirtualRide",
+	"VirtualRun",
+	"Walk",
+	"WeightTraining",
+	"Wheelchair",
+	"Windsurf",
+	"Workout",
+	"Yoga",
+]
+
+@dataclass
+class ApiActivity:
+	id: int
+	achievement_count: int | None
+	"""
+	The number of achievements gained during this activity
+	"""
+	athlete_id: int | None
+	athlete_count: int | None
+	"""
+	The number of athletes for taking part in a group activity
+	"""
+	average_speed: float | None
+	"""
+	The activity's average speed, in meters per second
+	"""
+	average_watts: float | None
+	"""
+	Average power output in watts during this activity. Rides only
+	"""
+	comment_count: int | None
+	"""
+	The number of comments for this activity
+	"""
+	commute: bool | None
+	"""
+	Whether this activity is a commute
+	"""
+	device_name: str | None
+	"""
+	The name of the device used to record the activity
+	"""
+	device_watts: bool | None
+	"""
+	Whether the watts are from a power meter, false if estimated
+	"""
+	distance: float | None
+	"""
+	The activity's distance, in meters
+	"""
+	elapsed_time: int | None
+	"""
+	The activity's elapsed time, in seconds
+	"""
+	elev_high: float | None
+	"""
+	The activity's highest elevation, in meters
+	"""
+	elev_low: float | None
+	"""
+	The activity's lowest elevation, in meters
+	"""
+	end_lat: float | None
+	end_long: float | None
+	external_id: str | None
+	"""
+	The identifier provided at upload time
+	"""
+	flagged: bool | None
+	"""
+	Whether this activity is flagged
+	"""
+	gear_id: str | None
+	"""
+	The id of the gear for the activity
+	"""
+	has_kudoed: bool | None
+	"""
+	Whether the logged-in athlete has kudoed this activity
+	"""
+	hide_from_home: bool | None
+	"""
+	Whether the activity is muted
+	"""
+	kilojoules: float | None
+	"""
+	The total work done in kilojoules during this activity. Rides only
+	"""
+	kudos_count: int | None
+	"""
+	The number of kudos given for this activity
+	"""
+	manual: bool | None
+	"""
+	Whether this activity was created manually
+	"""
+	map_id: str | None
+	"""
+	The identifier of the map
+	"""
+	map_polyline: str | None
+	"""
+	The polyline of the map, only returned on detailed representation of an object
+	"""
+	map_summary_polyline: str | None
+	"""
+	The summary polyline of the map
+	"""
+	max_speed: float | None
+	"""
+	The activity's max speed, in meters per second
+	"""
+	max_watts: int | None
+	"""
+	Rides with power meter data only
+	"""
+	moving_time: int | None
+	"""
+	The activity's moving time, in seconds
+	"""
+	name: str | None
+	"""
+	The name of the activity
+	"""
+	photo_count: int | None
+	"""
+	The number of Instagram photos for this activity
+	"""
+	private: bool | None
+	"""
+	Whether this activity is private
+	"""
+	sport_type: SportType | None
+	start_date: datetime | None
+	"""
+	The time at which the activity was started.
+	"""
+	start_date_local: datetime | None
+	"""
+	The time at which the activity was started in the local timezone.
+	"""
+	start_lat: float | None
+	start_long: float | None
+	timezone: str | None
+	"""
+	The timezone of the activity
+	"""
+	total_elevation_gain: float | None
+	"""
+	The activity's total elevation gain.
+	"""
+	total_photo_count: int | None
+	"""
+	The number of Instagram and Strava photos for this activity
+	"""
+	trainer: bool | None
+	"""
+	Whether this activity was recorded on a training machine
+	"""
+	type: ActivityType | None
+	"""
+	Deprecated. Prefer to use sport_type
+	"""
+	upload_id: int | None
+	"""
+	The identifier of the upload that resulted in this activity
+	"""
+	upload_id_str: str | None
+	"""
+	The unique identifier of the upload in string format
+	"""
+	weighted_average_watts: int | None
+	"""
+	Similar to Normalized Power. Rides with power meter data only
+	"""
+	workout_type: int | None
+	"""
+	The activity's workout type
+	"""
+	calories: float | None
+	"""
+	The number of kilocalories consumed during this activity
+	"""
+	description: str | None
+	"""
+	The description of the activity
+	"""
+	device_name: str | None
+	"""
+	The name of the device used to record the activity
+	"""
+	embed_token: str | None
+	"""
+	The token used to embed a Strava activity
+	"""
+	gear_distance: float | None
+	"""
+	The distance logged with this gear.
+	"""
+	gear_id: str | None
+	"""
+	The gear's unique identifier.
+	"""
+	gear_name: str | None
+	"""
+	The gear's name.
+	"""
+	photos_count: int | None
+	photos_primary_id: int | None
+	photos_urls: str | None
